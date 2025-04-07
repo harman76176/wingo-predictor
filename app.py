03595ff6f8e1333087830037a49219d47b89096c
harman76176 <devilhunter76176@gmail.com>
2025-04-07 19:02:51 +0530

    Removed Selenium, using mock results in scraper

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
 import pickle
-from scraper import get_latest_results
 import numpy as np
 
 app = Flask(__name__)
@@ -9,7 +8,8 @@ model = pickle.load(open("wingo_model.pkl", "rb"))
 
 @app.route("/")
 def home():
-    last = get_latest_results()
+    # Live scraping hata diya hai, dummy data use kar rahe
+    last = [3, 7, 5]  # temporary
     x_input = np.array(last).reshape(1, -1)
     predicted = model.predict(x_input)[0]
     return render_template("index.html", last=last, prediction=predicted)
